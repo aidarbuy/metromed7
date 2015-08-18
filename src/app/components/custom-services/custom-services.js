2804bba49ea8407d41b6c744b91ef87d0d6b17f0
aidarbuy <aidarbuy@gmail.com>
2015-08-18 20:40:48 +0600

    Added styles for top-sidebar, quote and custom-services

--- src/app/components/custom-services/custom-services.js
+++ src/app/components/custom-services/custom-services.js
@@ -4,28 +4,32 @@ var customServices = [
 		title: "Cardio Health",
 		text: [
 			"Our clinic is equipped with EKG, X-ray and Laboratory."
-		]
+		],
+		href: ''
 	},
 	{
 		icon: "local_hospital",
 		title: "Medical Treatment",
 		text: [
 			"We administer breathing treatments, repair lacerations, dispense medications intravenously, remove foreign objects from the eyes, and more."
-		]
+		],
+		href: ''
 	},
 	{
 		icon: "photo_camera",
 		title: "Photo Gallery",
 		text: [
 			"Check out our pictures!"
-		]
+		],
+		href: ''
 	},
 	{
 		icon: "accessibility",
-		title: "Aging Clinic",
+		title: "Anti-aging Institute",
 		text: [
 			"We organize regular meetings with clients who want to be healthy, youthful and energetic."
-		]
+		],
+		href: 'http://cognitivemuse.com/'
 	},
 ];
 
